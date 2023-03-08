1b8a81bf2a51f4f581cf35dad8e530dd3f2e958a
Andromeda <79194733+BlueCyborg@users.noreply.github.com>
2023-03-08 12:00:48 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -31,7 +31,7 @@ var div = document.getElementById("write");
  * 
  * @type {number}
  */
-var stepInterval = setInterval(() => { step(); }, 50);
+var stepInterval = setInterval(() => { step(); }, 5000);
 
 /**
  * ID de l'intervalle utilisé pour supprimer des lettres du mot actuellement affiché.
