33e7717d99f88159f416748e121aa7b72e281ae2
Mathys Ferre <mathysferre@MacBook-Pro-de-Mathys.local>
2023-03-12 22:34:13 +0100

    Ajout de fonctionnalités (barre chargement, liste numérotée)

---
+++ scripts/list.js
@@ -0,0 +1,12 @@
+var listElm = document.querySelector('#infinite-list');
+// Add 20 items.
+var nextItem = 1;
+var loadMore = function () {
+    for (var i = 0; i < 30; i++) {
+        var item = document.createElement('li');
+        item.innerText = nextItem++;
+        listElm.appendChild(item);
+    }
+}
+// Initially load some items.
+loadMore();
