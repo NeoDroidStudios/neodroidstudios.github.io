58942f343e111fbf7208152e5c655f139cb61a27
BlueCyborg <mathys.ferre@outlook.fr>
2023-03-23 16:59:31 +0100

    Ajout de nouvelles fonctionnalités

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -2,7 +2,7 @@ var listElm = document.querySelector('#infinite-list');
 // Add 20 items.
 var nextItem = 1;
 var loadMore = function () {
-    for (var i = 0; i < 30; i++) {
+    for (var i = 0; i < 25; i++) {
         var item = document.createElement('li');
         item.innerText = nextItem++;
         listElm.appendChild(item);
