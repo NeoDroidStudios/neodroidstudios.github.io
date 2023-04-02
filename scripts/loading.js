05e6543950db9919dc6969caca311f69dd5d9dd3
BlueCyborg <mathys.ferre@outlook.fr>
2023-04-02 19:14:09 +0200

    Ajout du cookie

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,7 +1,19 @@
-//Permet d'afficher la barre de chargement pendant 3,7 secondes puis d'afficher la page
-document.getElementById('main').style.display = "none";
-function chargement() {
+//Le cookie permet de n'afficher d'une fois la barre de chargement
+if (typeof (cookie) === 'undefined') {   
+    //Permet d'afficher la barre de chargement pendant 3,7 secondes puis d'afficher la page
+    document.getElementById('main').style.display = "none";
+    function chargement() {
+        document.getElementById('loader').remove();
+        document.getElementById('main').style.display = "";
+    }
+    setTimeout(chargement, 3700);
+
+    var now = new Date();
+    var time = now.getTime();
+    //Expiration après une heure
+    time += 3600 * 1000;
+    now.setTime(time);
+    document.cookie = 'username=set; expires=' + now.toUTCString() + '; path=/';
+} else {
     document.getElementById('loader').remove();
-    document.getElementById('main').style.display = "";
 }
-setTimeout(chargement, 3700);
