7449ec4885471305e6a34f5bef46624a26a065c6
Andromeda <79194733+BlueCyborg@users.noreply.github.com>
2023-04-06 10:22:32 +0200

    Update scroll.js
    Modification of the scroll.js function which now allows an infinite sccoll

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,12 +1,24 @@
 var listElm = document.querySelector('#infinite-list');
-// Add 20 items.
 var nextItem = 1;
-var loadMore = function () {
-    for (var i = 0; i < 25; i++) {
-        var item = document.createElement('li');
-        item.innerText = nextItem++;
-        listElm.appendChild(item);
-    }
+
+var loadMore = function (numItems) {
+  for (var i = 0; i < numItems; i++) {
+    var item = document.createElement('li');
+    item.innerText = nextItem++;
+    listElm.appendChild(item);
+  }
 }
+
 // Initially load some items.
-loadMore();
+loadMore(25);
+
+window.addEventListener('scroll', function() {
+  var windowHeight = window.innerHeight;
+  var documentHeight = document.documentElement.scrollHeight;
+  var scrollTop = window.scrollY;
+
+  if (windowHeight + scrollTop >= documentHeight) {
+    // Load more items
+    loadMore(10); // Charger 10 éléments supplémentaires
+  }
+});
