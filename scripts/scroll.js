6f51a99b3cfca9f5cc335df29237fb520e0b89d0
BlueCyborg <mathys.ferre@outlook.fr>
2023-04-10 22:36:22 +0200

    Modification scoll.js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,5 +1,6 @@
 var listElm = document.querySelector('#infinite-list');
 var nextItem = 1;
+var listCounter = 0;
 
 var loadMore = function (numItems) {
   for (var i = 0; i < numItems; i++) {
@@ -7,18 +8,42 @@ var loadMore = function (numItems) {
     item.innerText = nextItem++;
     listElm.appendChild(item);
   }
+
+  // Incrémenter le compteur de listes
+  listCounter += numItems;
+
+  // Afficher un message après la 100ème liste
+  if (listCounter === 100) {
+    var message = document.createElement('p');
+    message.innerText = "Vous avez atteint la 100ème ligne, ça fait loin non ?";
+    message.style.textAlign = "center"; // Ajouter la propriété CSS pour centrer le texte
+    listElm.appendChild(message);
+  }
+  if (listCounter === 200) {
+    var messageFin = document.createElement('p');
+    messageFin.innerText = "Vous avez atteint la 200ème ligne, je crois que vous vous êtes perdu";
+    messageFin.style.textAlign = "center"; // Ajouter la propriété CSS pour centrer le texte
+    listElm.appendChild(messageFin);
+  }
 }
 
 // Initially load some items.
-loadMore(25);
+var initialItemCount = 30;
+if (window.innerWidth < 768) { // Pour les petits écrans
+  initialItemCount = 10;
+} else if (window.innerWidth < 992) { // Pour les écrans moyens
+  initialItemCount = 20;
+}
+loadMore(initialItemCount);
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   var windowHeight = window.innerHeight;
   var documentHeight = document.documentElement.scrollHeight;
   var scrollTop = window.scrollY;
 
   if (windowHeight + scrollTop >= documentHeight) {
     // Load more items
-    loadMore(10); // Charger 10 éléments supplémentaires
+    var itemsToLoad = 10;
+    loadMore(itemsToLoad);
   }
 });
